705e9490048a9f0817301b91876b84d80644973a
mostafa <mtgwely0100@gmail.com>
2022-10-25 12:26:23 +0200

    first commit

---
+++ src/main/java/com/example/demo/ClassConfig.java
@@ -0,0 +1,73 @@
+package com.example.demo;
+
+import okhttp3.*;
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.io.IOException;
+
+@Configuration
+@EnableScheduling
+public class ClassConfig {
+    private static OkHttpClient client = new OkHttpClient();
+    private static String getRandomUrl = "https://mostafa-memorize.herokuapp.com/word";
+    private String postUrl = "https://mostafa-memorize.herokuapp.com/word";
+    private static Request request = new Request.Builder().url(getRandomUrl).build();
+    static FCMService z = new FCMService();
+    static PushNotificationService y = new PushNotificationService(z);
+    static PushNotificationController x = new PushNotificationController(y);
+    static PushNotificationRequest h = new PushNotificationRequest();
+
+    @Scheduled(fixedDelay = 1000 * 60 )
+    public void sendMessage(){
+//        System.out.println("hello");
+        h.setToken("ccgDv7d7S3yMJ69H9kfLal:APA91bH51PQoV3cEablSTt5a_RzqcsGhGBMaBdrToQ2wKxs76TB1kRRVbUIMgeulrTco6D7c5WcowiPLbJlwxA7V76Y-ikAiF1jDA_kkkFMkYuKCwpsOdwroMGVPgMIkpBUZnjrtKz_8");
+        getRandomWord();
+
+    }
+
+
+    public static void getRandomWord(){
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                h.setTitle("Error: request faild");
+                h.setMessage(e.getMessage());
+                x.sendTokenNotification(h);
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+
+                final String MyRes = response.body().string();
+
+                JSONObject MyResJson = null;
+                try {
+                    MyResJson = new JSONObject(MyRes);
+                    String ss = MyResJson.getString("p").trim() + " : " + MyResJson.getString("word").trim() + "\n\n -" + MyResJson.getString("description") ;
+                    String title = MyResJson.getString("p").trim() + " : " + MyResJson.getString("word").trim();
+                    String content = MyResJson.getString("description");
+
+                    h.setTitle(title);
+                    h.setMessage(content);
+                    x.sendTokenNotification(h);
+
+                } catch (JSONException e) {
+                    h.setTitle("Error: onResponse");
+                    h.setMessage(e.getMessage());
+                    x.sendTokenNotification(h);
+                }
+
+
+
+            }
+        });
+
+
+    }
+}
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,68 +12,13 @@ import java.io.IOException;
 
 @SpringBootApplication
 public class DemoApplication {
-	private static OkHttpClient client = new OkHttpClient();
-	private static String getRandomUrl = "https://mostafa-memorize.herokuapp.com/word";
-	private String postUrl = "https://mostafa-memorize.herokuapp.com/word";
-	private static Request request = new Request.Builder().url(getRandomUrl).build();
-	static FCMService z = new FCMService();
-	static PushNotificationService y = new PushNotificationService(z);
-	static PushNotificationController x = new PushNotificationController(y);
-	static PushNotificationRequest h = new PushNotificationRequest();
+
 	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(DemoApplication.class, args);
-		h.setToken("ccgDv7d7S3yMJ69H9kfLal:APA91bH51PQoV3cEablSTt5a_RzqcsGhGBMaBdrToQ2wKxs76TB1kRRVbUIMgeulrTco6D7c5WcowiPLbJlwxA7V76Y-ikAiF1jDA_kkkFMkYuKCwpsOdwroMGVPgMIkpBUZnjrtKz_8");
 
-
-		//getRandomWord();
-
-		while (true){
-
-			getRandomWord();
-			Thread.sleep(1000 * 60 * 60);
-		}
 
 
 	}
 
-	public static void getRandomWord(){
-
-		client.newCall(request).enqueue(new Callback() {
-			@Override
-			public void onFailure(@NotNull Call call, @NotNull IOException e) {
-				h.setTitle("Error: request faild");
-				h.setMessage(e.getMessage());
-				x.sendTokenNotification(h);
-			}
-
-			@Override
-			public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-
-				final String MyRes = response.body().string();
-
-				JSONObject MyResJson = null;
-				try {
-					MyResJson = new JSONObject(MyRes);
-					String ss = MyResJson.getString("p").trim() + " : " + MyResJson.getString("word").trim() + "\n\n -" + MyResJson.getString("description") ;
-					String title = MyResJson.getString("p").trim() + " : " + MyResJson.getString("word").trim();
-					String content = MyResJson.getString("description");
-
-					h.setTitle(title);
-					h.setMessage(content);
-					x.sendTokenNotification(h);
-
-				} catch (JSONException e) {
-					h.setTitle("Error: onResponse");
-					h.setMessage(e.getMessage());
-					x.sendTokenNotification(h);
-				}
-
-
-
-			}
-		});
-
-
-	}
 
 }
